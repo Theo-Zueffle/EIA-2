42f37438b257ebbd3a2a5a0e90fd6d658534f9a3
Theo-Zueffle <80910252+Theo-Zueffle@users.noreply.github.com>
2021-10-14 15:01:47 +0200

    kleine schritte

--- A01_rdm.poem/randmpoem.js
+++ A01_rdm.poem/randmpoem.js
@@ -1,3 +1,19 @@
 var text = "Expecto Patronum";
-console.log(text);
+//console.log (text);
+var subjekt;
+subjekt = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+var praedikat;
+praedikat = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+var objekt;
+objekt = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+console.log(objekt, praedikat, objekt);
+for (var index = subjekt.length; index > 0; index--) {
+    getVerse(subjekt, praedikat, objekt);
+}
+function getVerse(_subjekt, _praedikat, _objekt) {
+    var vers = "";
+    var zufallsZahl = Math.floor(Math.random() * subjekt.length);
+    console.log(zufallsZahl);
+    return ("Alhonjb");
+}
 //# sourceMappingURL=randmpoem.js.map
--- A01_rdm.poem/randmpoem.ts
+++ A01_rdm.poem/randmpoem.ts
@@ -1,3 +1,23 @@
-var text: string ="Expecto Patronum";
-console.log (text);
+let text: string = "Expecto Patronum";
+//console.log (text);
 
+let subjekt: string[]; 
+subjekt = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"]; 
+
+let praedikat: string[]; 
+praedikat = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"]; 
+
+let objekt: string[]; 
+objekt = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+console.log(objekt, praedikat, objekt);
+
+for (let index: number = subjekt.length; index > 0 ; index --) {
+getVerse (subjekt, praedikat, objekt);
+} 
+function getVerse (_subjekt: string[], _praedikat: string[], _objekt: string[]): string {
+let vers: string = "";
+let zufallsZahl: number = Math.floor(Math.random() * subjekt.length);
+console.log(zufallsZahl);
+return ("Alhonjb");
+
+}
