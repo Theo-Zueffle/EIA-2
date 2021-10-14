3e86d106a0a2b9d42a15124ca9a6456121e55d6a
Theo-Zueffle <80910252+Theo-Zueffle@users.noreply.github.com>
2021-10-14 15:39:08 +0200

    endlich fertig

--- A01_rdm.poem/randmpoem.js
+++ A01_rdm.poem/randmpoem.js
@@ -6,14 +6,17 @@ var praedikat;
 praedikat = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
 var objekt;
 objekt = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-console.log(objekt, praedikat, objekt);
+//console.log(objekt, praedikat, objekt);
 for (var index = subjekt.length; index > 0; index--) {
     getVerse(subjekt, praedikat, objekt);
 }
 function getVerse(_subjekt, _praedikat, _objekt) {
     var vers = "";
-    var zufallsZahl = Math.floor(Math.random() * subjekt.length);
-    console.log(zufallsZahl);
-    return ("Alhonjb");
+    var zufallsZahlsubjekt = Math.floor(Math.random() * subjekt.length);
+    var zufallsZahlpraedikat = Math.floor(Math.random() * subjekt.length);
+    var zufallsZahlobjekt = Math.floor(Math.random() * subjekt.length);
+    vers = vers + _subjekt.splice(zufallsZahlsubjekt, 1) + " " + _praedikat.splice(zufallsZahlpraedikat, 1) + " " + _objekt.splice(zufallsZahlobjekt, 1);
+    console.log(vers);
+    return ("Aloha");
 }
 //# sourceMappingURL=randmpoem.js.map
--- A01_rdm.poem/randmpoem.ts
+++ A01_rdm.poem/randmpoem.ts
@@ -9,15 +9,17 @@ praedikat = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
 
 let objekt: string[]; 
 objekt = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-console.log(objekt, praedikat, objekt);
+//console.log(objekt, praedikat, objekt);
 
 for (let index: number = subjekt.length; index > 0 ; index --) {
 getVerse (subjekt, praedikat, objekt);
 } 
 function getVerse (_subjekt: string[], _praedikat: string[], _objekt: string[]): string {
 let vers: string = "";
-let zufallsZahl: number = Math.floor(Math.random() * subjekt.length);
-console.log(zufallsZahl);
-return ("Alhonjb");
-
+let zufallsZahlsubjekt: number = Math.floor(Math.random() * subjekt.length);
+let zufallsZahlpraedikat: number = Math.floor(Math.random() * subjekt.length);
+let zufallsZahlobjekt: number = Math.floor(Math.random() * subjekt.length);
+vers = vers + _subjekt.splice(zufallsZahlsubjekt, 1) + " " + _praedikat.splice(zufallsZahlpraedikat, 1) + " " + _objekt.splice(zufallsZahlobjekt, 1);
+console.log(vers);
+return ("Aloha");
 }
